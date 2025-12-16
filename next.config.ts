d41ad05add78d68f0c10a5aec3998110f5a119dc
rinesro <hamsanzah@gmail.com>
2025-12-16 23:11:14 +0700

    Remove output file tracing configuration
    Removed outputFileTracingRoot and outputFileTracingIncludes from next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./lib/generated/prisma/*"],
-    "/*": ["./lib/generated/prisma/*"],
-  },
+  
 };
 
 export default nextConfig;
